8a1a080e10189bd5ee13b44b18656b58a4b16aba
varvarian1 <varvarian1@mail.ru>
2024-08-20 13:06:12 +0300

    add display system uptime

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,31 @@ use sysinfo::{System};
 
 fn main() {
     let mut system = System::new_all();
-
     system.refresh_all();
 
+    display_cpu_usage(&system);
+    display_system_uptime(&system);
+    display_memory_usage(&system);
+}
+//fix
+fn display_system_uptime(system: &System) {
+    println!("\nSystem Uptime:");
+    let uptime = sysinfo::System::uptime();
+    println!("Uptime: {} seconds", uptime);
+}
+
+fn display_cpu_usage(system: &System) {
     println!("CPU Usage:");
     for (i, cpu) in system.cpus().iter().enumerate() {
         println!("CPU{}: {:.2}%", i, cpu.cpu_usage());
     }
+}
 
+
+fn display_memory_usage(system: &System) {
     println!("\nMemory Usage:");
-    let total_memory_gb = system.total_memory() / 1_048_576;
-    let used_memory_gb = system.used_memory() / 1_048_576;   
+    let total_memory_gb = system.total_memory() / 1_048_576; 
+    let used_memory_gb = system.used_memory() / 1_048_576; 
     println!("Total memory: {} GB", total_memory_gb);
     println!("Used memory: {} GB", used_memory_gb);
 }
