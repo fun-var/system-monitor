6df88acf62bd4635a956d30e5950551a65d4b7d2
varvarian1 <varvarian1@mail.ru>
2024-08-21 09:24:09 +0300

    add system refresh

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,21 @@
+use std::thread;
+use std::time::Duration;
+
 use sysinfo::{System};
 
 fn main() {
     let mut system = System::new_all();
-    system.refresh_all();
-
-    display_cpu_usage(&system);
-    display_system_uptime(&system);
-    display_memory_usage(&system);
+    
+    loop {
+        system.refresh_all();
+        
+        display_cpu_usage(&system);
+        display_system_uptime(&system);
+        display_memory_usage(&system);
+        thread::sleep(Duration::from_secs((5)));
+    }
 }
-//fix
+
 fn display_system_uptime(system: &System) {
     println!("\nSystem Uptime:");
     let uptime = sysinfo::System::uptime();
@@ -22,11 +29,10 @@ fn display_cpu_usage(system: &System) {
     }
 }
 
-
 fn display_memory_usage(system: &System) {
     println!("\nMemory Usage:");
-    let total_memory_gb = system.total_memory() / 1_048_576; 
-    let used_memory_gb = system.used_memory() / 1_048_576; 
+    let total_memory_gb = system.total_memory() / 1_073_741_824; 
+    let used_memory_gb = system.used_memory() / 1_073_741_824; 
     println!("Total memory: {} GB", total_memory_gb);
     println!("Used memory: {} GB", used_memory_gb);
 }
